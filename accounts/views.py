5e81e3ebcc0f3424fca51fc05b4df147bf66e229
Priyanka Kannan <priyankannan91@gmail.com>
2023-07-24 16:22:04 -0500

    Feature 5 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -33,3 +34,32 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
+
+# Signup
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # data is stored in a dictionary for retrevial
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+            if password == password_confirmation:
+                # create a new login and save info to properties
+                user = User.objects.create_user(
+                    username,
+                    password,
+                )
+                # login user
+                login(request, user)
+                # redirect to home page
+                return redirect("home")
+            else:
+                form.add_error("password", "Password do not match")
+    else:
+        form = SignupForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
